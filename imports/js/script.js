a0a6e5e1e8a007097c2d816c9ef38621a91f54ea
unknown <>
2015-10-25 18:08:07 -0400

    added import map

--- imports/js/script.js
+++ imports/js/script.js
@@ -1,5 +1,45 @@
 // http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames  FOR MORE BASEMAP TILES
 
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function makeTooltip(layer,data,dataType,year){
+	for (var i = 0; i < $('.cartodb-tooltip').length; i++) {
+		if(i!=1){
+			$('.cartodb-tooltip')[i].remove();
+		}	
+	}
+	
+	var yearString = dataType+year;
+	var tooltipInfoArray = [
+		data.cntry_name,
+		data[yearString]
+	];
+	var tooltipHTML = tooltipInfoArray.map(function(elem,idx){
+		// var suffix = elem.length > 3 ? 'b' : 'm'; // If `elem` is longer than three, it's a number like 1000, or $1 billion.
+		var txt;
+		if(idx===0){
+			txt = '<b>'+elem+'</b>';
+		} else {
+			elem = elem*1000000
+			// var suffix  = elem < 1000000000 ? 'm' : 'b';
+			txt = 'Weapons imports: $'+numberWithCommas(elem);
+		}
+		// var txt = idx===0 ? '<b>'+elem+'</b>' : 'Weapons imports: $'+numberWithCommas(elem)+'m';
+		return '<p>'+txt+'</p>';
+	}).join('');
+
+	tooltip = new cdb.geo.ui.Tooltip({
+	    layer: layer,
+	    template: tooltipHTML, 
+	    width: 200,
+	    position: 'bottom|right'
+	});
+					
+	$('body').append(tooltip.render().el);
+}
+
 window.onload = function(){
 	var cartoDbTableName = 'sipri_import_export_map_1950_2014';
 	var domId = 'map';
@@ -32,44 +72,68 @@ window.onload = function(){
 	cartodb.createLayer(mapObject,layerSource)
 		.addTo(mapObject)
 		.on('done',function(layer){
+			var tooltip;
+			var sublayer = layer.getSubLayer(0);
+			sublayer.setInteraction(true);
+			sublayer.setInteractivity('cntry_name, importyear1950');
+			sublayer.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
+				makeTooltip(layer,data,'importyear',1950);
+			}).on('error',function(err){
+				console.log('Error featureOver: '+err);
+			});
+			
+			sublayer.on('featureOut',function(e, latlng, pos, data, subLayerIndex){
+				$('.cartodb-tooltip').remove();
+			}).on('error',function(err){
+				console.log('Error featureOut: '+err);
+			});
+
 			$('#ex1').on('slide',function(slideEvent){
 				var year = slideEvent.value;
 				$('#ex6SliderVal').text(year);
-				layer.getSubLayer(0).setSQL("SELECT * FROM "+cartoDbTableName+" WHERE (importyear"+year+">0 AND gwsyear<="+year+" AND gwsyear>0)");
-				layer.getSubLayer(0).setCartoCSS(
-					"#sipri_import_export_map_1950_2014{" +
+				sublayer.setSQL("SELECT * FROM "+cartoDbTableName+" WHERE (importyear"+year+">0 AND gwsyear<="+year+" AND gwsyear>0)");
+				sublayer.setCartoCSS(
+					"#sipri_import_import_map_1950_2014{" +
 					  "polygon-fill: #dddddd;" +
 					  "polygon-opacity: 1;" +
 					  "line-color: #FFF;" +
 					  "line-width: 0.5;" +
 					  "line-opacity: 1;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" > 1000] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" > 1000] {" +
 					   "polygon-fill: #B10026;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 1000] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 1000] {" +
 					   "polygon-fill: #E31A1C;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 800] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 800] {" +
 					   "polygon-fill: #FC4E2A;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 600] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 600] {" +
 					   "polygon-fill: #FD8D3C;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 400] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 400] {" +
 					   "polygon-fill: #FEB24C;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 200] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 200] {" +
 					   "polygon-fill: #FED976;" +
 					"}" +
-					"#sipri_import_export_map_1950_2014 [ importyear"+year+" <= 20] {" +
+					"#sipri_import_import_map_1950_2014 [ importyear"+year+" <= 20] {" +
 					   "polygon-fill: #FFFFB2;" +
-					"}"
-				);
-				layer.getSubLayer(0).setInteraction(true);
-				layer.getSubLayer(0).infowindow.set('template', $('#infowindow-template').html());
-				layer.getSubLayer(0).on('mouseover', function(e,latlng,pos,data){
-					console.log(data);
+					"}");
+				
+				sublayer.setInteractivity('cntry_name, importyear'+year);
+				
+				sublayer.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
+					makeTooltip(layer,data,'importyear',year);
+				}).on('error',function(err){
+					console.log('Error: '+err);
+				});
+
+				sublayer.on('featureOut',function(e, latlng, pos, data, subLayerIndex){
+					$('.cartodb-tooltip').remove();
+				}).on('error',function(err){
+					console.log('Error featureOut: '+err);
 				});
 			});
 		}).on('error',function(err){
@@ -91,7 +155,7 @@ window.onload = function(){
 
 	rangeLow.className = 'legend-range';
 	rangeHigh.className = 'legend-range';
-	rangeLow.innerHTML = '$20,000 or less';
+	rangeLow.innerHTML = '$20 million or less';
 	rangeHigh.innerHTML = 'More than $1 billion';
 
 	legendWrapper.appendChild(rangeHigh);
@@ -106,4 +170,8 @@ window.onload = function(){
 	}
 
 	legendWrapper.appendChild(rangeLow);
+
+	var note = document.createElement('div');
+	note.innerHTML = 'All figures in 2014 inflation-adjusted U.S. dollars.';
+	legendWrapper.appendChild(note);
 };
